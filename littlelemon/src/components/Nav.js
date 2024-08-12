72fee83ae4ec4e67f8b968b46ade95c863c46152
caroc24 <carolinacamarda@gmail.com>
2024-08-12 18:20:57 -0400

    Added mobile version to nav bar.

--- littlelemon/src/components/Nav.js
+++ littlelemon/src/components/Nav.js
@@ -5,19 +5,27 @@ export default function Nav(){
     const toggleMenu = () =>{
         setMenuOpen(!menuOpen);
     }
-    return <nav className = {`navbar`}>
-        <img src = "logo.jpg" height={50} alt="Little Lemon logo"/>
-        <div className="navlinks">
-            <ul>
-                <li><a href="/home">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/menu">Menu</a></li>
-                <li><a href="/reservations">Reservations</a></li>
-                <li><a href="/orderonline">Order Online</a></li>
-                <li><a href="/login">Login</a></li>
-            </ul>
+    return <nav className = {`navbar ${menuOpen ? "open" : ""}`}>
+        <a href="/" classname="logo">
+            <img src = "logo.jpg" height={50} alt="Little Lemon logo"/>
+        </a>
+
+        {/*Mobile Navbar*/}
+        <div className="menu-icon" onClick={toggleMenu}>
+            <div className="bar"></div>
+            <div className="bar"></div>
+            <div className="bar"></div>
         </div>
 
+        {/*Navigation Bar Items*/}
+        <ul className={`nav-links ${menuOpen ? "visible" : ""}`}>
+            <li><a href="/home">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/menu">Menu</a></li>
+            <li><a href="/reservations">Reservations</a></li>
+            <li><a href="/orderonline">Order Online</a></li>
+            <li><a href="/login">Login</a></li>
+        </ul>
     </nav>
 }
 
