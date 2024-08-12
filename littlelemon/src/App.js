3164baf56d9942b7121b7c27d3d6594f6a8425bf
caroc24 <carolinacamarda@gmail.com>
2024-08-12 15:35:45 -0400

    Added basic CSS

--- littlelemon/src/App.js
+++ littlelemon/src/App.js
@@ -1,13 +1,22 @@
+import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <react>
       <meta name="description" content="The Little Lemon restaurant proudly serves mediterranean family recipes with a modern twist." />
-      <meta name="og:title" content="Little Lemon Restaurant"/>
-      <meta name="og:description" content="The Little Lemon restaurant app allows customers to view the menu, make reservations, and order online."/>
+      <meta name="og:type" content="website"/>
       <meta name="og:image" content="logo.jpg"/>
-      
+      <meta name="og:url" content="https://littlelemon"/>
+      <meta name="og:title" content="Little Lemon Restaurant"/>
+      <meta name="og:description" content="Little Lemon is a family-owned Mediterranean restaurant that mixes family recipes with a modern twist."/>
+      <meta name="og:locale" content="en_US"/>
+      <meta name="og:site_name" content="Little Lemon"/>
+
       <Header/>
       <Nav/>
       <Main/>
---
+++ littlelemon/src/components/Footer.js
@@ -0,0 +1,28 @@
+export default function Footer(){
+    return <footer className = "footer">
+        {/* <img src = "logo.jpg" alt="Little Lemon logo"/> */}
+        <h4>Doormat Navigation</h4>
+        <ul>
+            <li><a href="/home">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/menu">Menu</a></li>
+            <li><a href="/reservations">Reservations</a></li>
+            <li><a href="/orderonline">Order Online</a></li>
+            <li><a href="/login">Login</a></li>
+        </ul>
+
+        <h4>Contact</h4>
+        <ul>
+            <li><a href="/address">Address</a></li>
+            <li><a href="/phonenumber">Phone Number</a></li>
+            <li><a href="/email">Email</a></li>
+        </ul>
+
+        <h4>Social Media Links</h4>
+        <ul>
+            <li><a href="/address">Address</a></li>
+            <li><a href="/phonenumber">Phone Number</a></li>
+            <li><a href="/email">Email</a></li>
+        </ul>
+    </footer>
+}
