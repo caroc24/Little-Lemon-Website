f41b6d4aee8a2d186ff3bf7d88ffb63d7846c6c1
caroc24 <carolinacamarda@gmail.com>
2024-08-12 18:29:40 -0400

    Added text to Header.js

--- littlelemon/src/components/Header.js
+++ littlelemon/src/components/Header.js
@@ -1,5 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 export default function Header(){
     return <header className = "header">
-        
+        <section>
+            {/* Banner Texts */}
+            <div className='banner'>
+                <h2>Little Lemon</h2>
+                <h3>Chicago</h3>
+                <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+                <Link to="/booking"><button aria-label='On Click'>Reserve Table</button></Link>
+            </div>
+            {/* Banner Image */}
+            <div>
+                <img src="" alt=""/>
+            </div>
+        </section>
     </header>
 }
