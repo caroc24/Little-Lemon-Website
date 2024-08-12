19c7fe2887b9f73e48835d9ae53bea3628962a1a
caroc24 <carolinacamarda@gmail.com>
2024-08-12 19:38:58 -0400

    Updated code in Booking.js

--- littlelemon/src/components/Booking.js
+++ littlelemon/src/components/Booking.js
@@ -1,5 +1,8 @@
-export default function Booking(){
-    return <booking className = "booking">
-        
-    </booking>
+import React from 'react';
+import BookingForm from './BookingForm';
+
+export default function Booking(props){
+    return (
+        <BookingForm availableTimes={props.availableTimes} dispatch ={props.dispatch} SubmitForm = {props.SubmitForm}/>
+    );
 }
