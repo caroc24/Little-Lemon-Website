d8d0033324f9f51cb900378c3ffeb57bbc3683d9
caroc24 <carolinacamarda@gmail.com>
2024-08-12 12:39:07 -0400

    Created new components and updated App.js

--- littlelemon/src/App.js
+++ littlelemon/src/App.js
@@ -2,9 +2,17 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Homepage</h1>
-    </div>
+    <react>
+      <meta name="description" content="The Little Lemon restaurant proudly serves mediterranean family recipes with a modern twist." />
+      <meta name="og:title" content="Little Lemon Restaurant"/>
+      <meta name="og:description" content="The Little Lemon restaurant app allows customers to view the menu, make reservations, and order online."/>
+      <meta name="og:image" content="logo.jpg"/>
+      
+      <Header/>
+      <Nav/>
+      <Main/>
+      <Footer/>
+    </react>
   );
 }
 
