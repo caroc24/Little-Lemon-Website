acde7c0315a42010a8d272ed3a08cfd2fc8eeafe
caroc24 <carolinacamarda@gmail.com>
2024-08-14 12:32:16 -0400

    updated Main.js

--- littlelemon/src/components/Main.js
+++ littlelemon/src/components/Main.js
@@ -4,55 +4,62 @@ import Booking from "./Booking";
 import ConfirmedBooking from "./ConfirmedBooking";
 import Header from "./Header";
 
+
 const Main = () => {
 
-    const seedRandom = function(seed){
-        var m = 2**35 -31;
+    // const [availableTimes, setAvailableTimes] = useState(["17:00", "18:00", "19:00", "20:00", "21:00", "22:00"])
+
+    //Chrome was blocking running the script on the index page so I added it here. "https://chromestatus.com/feature/5629709824032768"
+    const seededRandom = function (seed) {
+        var m = 2**35 - 31;
         var a = 185852;
         var s = seed % m;
-        return function(){
+        return function () {
             return (s = s * a % m) / m;
-        }
+        };
     }
-    const fetchAPI = function(date){
+
+    const fetchAPI = function(date) {
         let result = [];
-        let random = seedRandom(date.getDate());
-        for (let i = 17; i <= 23; i++) {
-            if(random() < 0.5){
+        let random = seededRandom(date.getDate());
+
+        for(let i = 17; i <= 23; i++) {
+            if(random() < 0.5) {
                 result.push(i + ':00');
             }
-            if(random() > 0.5){
+            if(random() < 0.5) {
                 result.push(i + ':30');
             }
         }
         return result;
-    }
-
-    const submitAPI = function(formData){
+    };
+    const submitAPI = function(formData) {
         return true;
-    }
-    const initialState = {availableTimes: fetchAPI(new Date())};
+    };
+
+    const initialState = {availableTimes:  fetchAPI(new Date())}
     const [state, dispatch] = useReducer(updateTimes, initialState);
 
-    function updateTimes(state, date){
-        return {availableTimes: fetchAPI(new Date())}
+    function updateTimes(state, date) {
+        return {availableTimes: fetchAPI(new Date(date))}
     }
-
     const navigate = useNavigate();
-
-    function submitForm (formData){
-        if(submitAPI(formData)){
-            navigate("/confirmed");
+    function submitForm (formData) {
+        if (submitAPI(formData)) {
+            navigate("/confirmed")
         }
     }
-    return (
+
+    return(
         <main className="main">
             <Routes>
                 <Route path="/" element={<Header />} />
                 <Route path="/booking" element={<Booking availableTimes={state} dispatch={dispatch} submitForm={submitForm}/>} />
-                <Route path="/confirmed" element={<ConfirmedBooking/>}/>
+                <Route path="/confirmed" element={<ConfirmedBooking/> } />
             </Routes>
         </main>
+
+
     )
 }
 
