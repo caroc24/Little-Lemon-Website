595d7b78f2bbcca7533cb4e83a0e72e33162a21c
caroc24 <carolinacamarda@gmail.com>
2024-08-14 12:54:21 -0400

    updated menu button

--- littlelemon/src/components/Menu.js
+++ littlelemon/src/components/Menu.js
@@ -1,31 +1,31 @@
-import React from 'react';
-import recipes from '../recipes';
+import React from "react";
+import recipes from "../recipes";
 
 const Menu = () => {
-    return (
-        <div className = 'menu-container'>
-            <div className = 'menu-header'>
-                <h2>This week's specials!</h2>
-                <button>Order Menu</button>
+//   console.log(recipes[0]);
+  return (
+    <div className="menu-container">
+      <div className="menu-header">
+        <h2>This weeks specials!</h2>
+        <button>Online Menu</button>
+      </div>
+      <div className="cards">
+        {recipes.map((recipe) => (
+          <div key={recipe.id} className="menu-items">
+            <img src={recipe.image} alt="" />
+            <div className="menu-content">
+              <div className="heading">
+                <h5>{recipe.title}</h5>
+                <p>${recipe.price}</p>
+              </div>
+              <p>{recipe.description}</p>
+              <button className="orderbtn">Order Now</button>
             </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-            /* Menu Cards */
-            <div>
-                {
-                    recipes.map(recipe => <div key = {recipe.id}>
-                        <img src={recipe.image} alt =''/>
-                        <div>
-                            <div>
-                                <h5>{recipe.title}</h5>
-                                <p>{recipe.price}</p>
-                            </div>
-                            <p>{recipe.description}</p>
-                        </div>
-                    </div>
-                    )
-                }
-            </div>
-        </div>
-    );
-}
 export default Menu;
